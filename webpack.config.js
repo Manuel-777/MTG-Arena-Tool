a1ed92cb4c80f2e3fdc3d5ab6d0c37ef417c8190
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2019-11-13 20:15:12 -0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+const isProduction = typeof NODE_ENV !== 'undefined' && NODE_ENV === 'production';
+const mode = isProduction ? 'production' : 'development';
+const devtool = isProduction ? false : 'inline-source-map';
+
+const baseConfig = {
+  target: 'node',
+  mode,
+  devtool,
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        enforce: 'pre',
+        loader: 'tslint-loader',
+        exclude: /node_modules/,
+        options: {
+          typeCheck: true,
+          emitErrors: true
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx', '.jsx', '.json']
+  }
+};
+
+module.exports = [
+  {...baseConfig,
+    entry: './src/main.js',
+    output: {
+      filename: 'main.js',
+      path: path.join(__dirname, 'dist')
+    }
+  },
+  {...baseConfig,
+    entry: './src/window_background/background.js',
+    output: {
+      filename: 'background.js',
+      path: path.join(__dirname, 'dist')
+    }
+  },
+  {...baseConfig,
+    entry: './src/window_main/renderer.js',
+    output: {
+      filename: 'renderer.js',
+      path: path.join(__dirname, 'dist')
+    }
+  },
+  {...baseConfig,
+    entry: './src/overlay/overlay.tsx',
+    output: {
+      filename: 'overlay.tsx',
+      path: path.join(__dirname, 'dist')
+    }
+  }
+];
