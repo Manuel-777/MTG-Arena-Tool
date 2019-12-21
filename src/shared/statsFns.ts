1218fe34de6bd70bf7ff0cd8c2d009a5bec2f620
Benjamin Lu <lu.s.benjamin@gmail.com>
2019-12-21 10:34:03 -0800

    typescript refactor

--- src/shared/statsFns.ts
+++ src/shared/statsFns.ts
@@ -1,15 +1,28 @@
-import { create, all } from "mathjs";
-const config = { precision: 2000 };
-const math = create(all, config);
+import { create, all, MathJsStatic } from "mathjs";
 
-//
+const config = { precision: 2000 };
+const math: MathJsStatic = create(all, config) as MathJsStatic;
+
 function hypergeometric(
-  exact,
-  population,
-  sample,
-  hitsInPop,
+  exact: number,
+  population: number,
+  sample: number,
+  hitsInPop: number
+): number;
+function hypergeometric(
+  exact: number,
+  population: number,
+  sample: number,
+  hitsInPop: number,
+  returnBig: boolean
+): math.MathType;
+function hypergeometric(
+  exact: number,
+  population: number,
+  sample: number,
+  hitsInPop: number,
   returnBig = false
-) {
+): math.MathType {
   return hypergeometricRange(
     exact,
     exact,
@@ -20,31 +33,46 @@ function hypergeometric(
   );
 }
 
-function hypergeometricRange(
-  lowerBound,
-  upperBound,
-  population,
-  sample,
-  hitsInPop,
+export function hypergeometricRange(
+  lowerBound: number,
+  upperBound: number,
+  population: number,
+  sample: number,
+  hitsInPop: number
+): number;
+export function hypergeometricRange(
+  lowerBound: number,
+  upperBound: number,
+  population: number,
+  sample: number,
+  hitsInPop: number,
+  returnBig: boolean
+): math.MathType;
+export function hypergeometricRange(
+  lowerBound: number,
+  upperBound: number,
+  population: number,
+  sample: number,
+  hitsInPop: number,
   returnBig = false
-) {
+): math.MathType {
   if (lowerBound > upperBound || lowerBound > hitsInPop) {
     return returnBig ? math.bignumber(0) : 0;
   }
 
-  let _population = math.bignumber(population);
-  let _sample = math.bignumber(sample);
-  let _hitsInPop = math.bignumber(hitsInPop);
-  let matchingCombos = math.bignumber(0);
+  const _population = math.bignumber(population);
+  const _sample = math.bignumber(sample);
+  const _hitsInPop = math.bignumber(hitsInPop);
+  let matchingCombos: math.MathType = math.bignumber(0);
   // Can't have more non-hits in the sample than exist in the population
   for (
     let i = math.max(lowerBound, sample - (population - hitsInPop));
     i <= upperBound && i <= sample;
     i++
   ) {
-    let _hitsInSample = math.bignumber(i);
-    let _hitCombos = math.combinations(_hitsInPop, _hitsInSample);
-    let _missCombos = math.combinations(
+    const _hitsInSample = math.bignumber(i);
+    const _hitCombos = math.combinations(_hitsInPop, _hitsInSample);
+    const _missCombos = math.combinations(
       math.max(0, math.subtract(_population, _hitsInPop)),
       math.max(0, math.subtract(_sample, _hitsInSample))
     );
@@ -54,19 +82,25 @@ function hypergeometricRange(
     );
   }
 
-  let totalCombos = math.combinations(_population, _sample);
-  let probability = math.divide(matchingCombos, totalCombos);
-  return returnBig ? probability : math.number(probability);
+  const totalCombos = math.combinations(_population, _sample);
+  const probability = math.divide(matchingCombos, totalCombos);
+  return returnBig ? probability : math.number(probability as any);
 }
 
-function hypergeometricSignificance(
-  value,
-  population,
-  sample,
-  hitsInPop,
+export function hypergeometricSignificance(
+  value: number,
+  population: number,
+  sample: number,
+  hitsInPop: number
+): number;
+export function hypergeometricSignificance(
+  value: number,
+  population: number,
+  sample: number,
+  hitsInPop: number,
   returnBig = false
-) {
-  let percentile = hypergeometricRange(
+): math.MathType {
+  const percentile = hypergeometricRange(
     0,
     value,
     population,
@@ -74,13 +108,13 @@ function hypergeometricSignificance(
     hitsInPop,
     true
   );
-  let chance = hypergeometric(value, population, sample, hitsInPop, true);
+  const chance = hypergeometric(value, population, sample, hitsInPop, true);
   if (math.smallerEq(percentile, 0.5)) {
-    let midpoint = math.subtract(percentile, math.divide(chance, 2));
-    let retVal = math.multiply(midpoint, 2);
-    return returnBig ? retVal : math.number(retVal);
+    const midpoint = math.subtract(percentile, math.divide(chance, 2));
+    const retVal = math.multiply(midpoint, 2);
+    return returnBig ? retVal : math.number(retVal as any);
   }
-  let reversePercentile = hypergeometricRange(
+  const reversePercentile = hypergeometricRange(
     value,
     math.min(hitsInPop, sample),
     population,
@@ -89,9 +123,9 @@ function hypergeometricSignificance(
     true
   );
   if (math.smallerEq(reversePercentile, 0.5)) {
-    let midpoint = math.subtract(reversePercentile, math.divide(chance, 2));
-    let retVal = math.multiply(midpoint, 2);
-    return returnBig ? retVal : math.number(retVal);
+    const midpoint = math.subtract(reversePercentile, math.divide(chance, 2));
+    const retVal = math.multiply(midpoint, 2);
+    return returnBig ? retVal : math.number(retVal as any);
   }
   // If we get here, then value is the median and we need to weight things for how off-center its percentile range is.
   let smaller, larger;
@@ -104,29 +138,34 @@ function hypergeometricSignificance(
   }
   // Divide the range into a symmetric portion centered on .5, and another portion for the rest. Calculate the average
   // distance from center for each, and use the average of that weighted by each portion's size.
-  let centeredSize = math.multiply(math.subtract(smaller, 0.5), 2);
-  let otherSize = math.subtract(larger, smaller);
-  let centeredAverage = math.divide(centeredSize, 4); // half for being centered, half again for average
+  const centeredSize = math.multiply(math.subtract(smaller, 0.5), 2);
+  const otherSize = math.subtract(larger, smaller);
+  const centeredAverage = math.divide(centeredSize, 4); // half for being centered, half again for average
   // Average of the farther bound (otherSize + centeredSize/2) and the closer bound (centeredSize/2). Works out to
   // ((otherSize + centeredSize/2) + (centeredSize/2)) / 2, simplified to (otherSize + centeredSize) / 2.
-  let otherAverage = math.divide(math.add(centeredSize, otherSize), 2);
-  let weightedAverage = math.divide(
+  const otherAverage = math.divide(math.add(centeredSize, otherSize), 2);
+  const weightedAverage = math.divide(
     math.add(
       math.multiply(centeredSize, centeredAverage),
       math.multiply(otherSize, otherAverage)
     ),
     chance
   );
-  let retVal = math.subtract(1, math.multiply(weightedAverage, 2));
-  return returnBig ? retVal : math.number(retVal);
+  const retVal = math.subtract(1, math.multiply(weightedAverage, 2));
+  return returnBig ? retVal : math.number(retVal as any);
 }
 
-export {
-  hypergeometricRange,
-  hypergeometricSignificance,
-  normalApproximationInterval
-};
-function normalApproximationInterval(matches, wins) {
+/**
+ * Computes the Wald Interval aka Normal Approximation Interval
+ * Useful for quickly estimating the 95% confidence interval
+ * Can produce bad results when sample-size < 20
+ * https://en.wikipedia.org/wiki/Binomial_proportion_confidence_interval#Normal_approximation_interval
+ * https://www.channelfireball.com/articles/magic-math-how-many-games-do-you-need-for-statistical-significance-in-playtesting/
+ */
+export function normalApproximationInterval(
+  matches: number,
+  wins: number
+): { winrate: number; interval: number } {
   if (!matches) return { winrate: 0, interval: 0 };
   const winrate = wins / matches;
   const interval = 1.96 * Math.sqrt((winrate * (1 - winrate)) / matches);
