194f99838f71b3a79fca13f52e03bb046b5e396e
Benjamin Lu <lu.s.benjamin@gmail.com>
2020-01-04 11:31:41 -0800

    extract common TableHeaders

--- src/window_main/components/decks/DecksTableControls.tsx
+++ src/window_main/components/decks/DecksTableControls.tsx
@@ -6,6 +6,7 @@ import { TABLE_MODES } from "../../../shared/constants";
 
 import FilterPanel from "../../FilterPanel";
 import PagingControls from "../PagingControls";
+import TableHeaders from "../TableHeaders";
 import {
   CheckboxContainer,
   SmallTextButton,
@@ -95,6 +96,14 @@ export default function DecksTableControls({
     setPageSize,
     pageIndex,
     pageSize
+  };
+  const headersProps = {
+    filtersVisible,
+    getTableProps,
+    gridTemplateColumns,
+    setFilter,
+    setFiltersVisible,
+    visibleHeaders
   };
   return (
     <>
@@ -219,77 +228,7 @@ export default function DecksTableControls({
           <PagingControls {...pagingProps} />
         </div>
       </div>
-      <div
-        className="decks_table_head line_dark"
-        style={{ gridTemplateColumns }}
-        {...getTableProps()}
-      >
-        {visibleHeaders.map((column: any, ii: number) => (
-          <div
-            {...column.getHeaderProps(column.getSortByToggleProps())}
-            className={"hover_label"}
-            style={{
-              height: "64px",
-              gridArea: `1 / ${ii + 1} / 1 / ${ii + 2}`
-            }}
-            key={column.id}
-          >
-            <div className={"decks_table_head_container"}>
-              <div
-                className={
-                  column.isSorted
-                    ? column.isSortedDesc
-                      ? " sort_desc"
-                      : " sort_asc"
-                    : ""
-                }
-                style={{ marginRight: "4px", width: "16px" }}
-              />
-              <div className={"flex_item"}>{column.render("Header")}</div>
-              {column.canFilter && (
-                <div
-                  style={{ marginRight: 0 }}
-                  className={"button settings"}
-                  onClick={(e): void => {
-                    e.stopPropagation();
-                    setFiltersVisible({
-                      ...filtersVisible,
-                      [column.id]: !filtersVisible[column.id]
-                    });
-                  }}
-                  title={
-                    (filtersVisible[column.id] ? "hide" : "show") +
-                    " column filter"
-                  }
-                />
-              )}
-              {column.filterValue && (
-                <div
-                  style={{ marginRight: 0 }}
-                  className={"button close"}
-                  onClick={(e): void => {
-                    e.stopPropagation();
-                    setFilter(column.id, undefined);
-                  }}
-                  title={"clear column filter"}
-                />
-              )}
-            </div>
-            {column.canFilter && filtersVisible[column.id] && (
-              <div
-                onClick={(e): void => e.stopPropagation()}
-                style={{
-                  display: "flex",
-                  justifyContent: "center"
-                }}
-                title={"filter column"}
-              >
-                {column.render("Filter")}
-              </div>
-            )}
-          </div>
-        ))}
-      </div>
+      <TableHeaders {...headersProps} />
     </>
   );
 }
