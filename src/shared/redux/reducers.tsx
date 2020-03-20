f5aa879ab3ec781cd83fa42e981c624050abe8f3
Benjamin Lu <lu.s.benjamin@gmail.com>
2020-03-20 14:43:31 -0700

    refactor collection redux slice

--- src/shared/redux/reducers.tsx
+++ src/shared/redux/reducers.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import { createSlice } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
 import { MergedSettings } from "../../types/settings";
@@ -168,7 +169,35 @@ export const homeSlice = createSlice({
     usersActive: 0
   },
   reducers: {
-    setHomeData: (state, action) => action.payload
+    setHomeData: (state, action): any => action.payload
+  }
+});
+
+export const collectionSlice = createSlice({
+  name: "collection",
+  initialState: {
+    countMode: "All cards",
+    rareDraftFactor: 3,
+    mythicDraftFactor: 0.14,
+    boosterWinFactor: 1.2,
+    futureBoosters: 0
+  },
+  reducers: {
+    setCountMode: (state, action): void => {
+      state.countMode = action.payload;
+    },
+    setRareDraftFactor: (state, action): void => {
+      state.rareDraftFactor = action.payload;
+    },
+    setMythicDraftFactor: (state, action): void => {
+      state.mythicDraftFactor = action.payload;
+    },
+    setBoosterWinFactor: (state, action): void => {
+      state.boosterWinFactor = action.payload;
+    },
+    setFutureBoosters: (state, action): void => {
+      state.futureBoosters = action.payload;
+    }
   }
 });
 
@@ -250,6 +279,7 @@ const rootReducer = combineReducers({
   hover: hoverSlice.reducer,
   login: loginSlice.reducer,
   homeData: homeSlice.reducer,
+  collection: collectionSlice.reducer,
   explore: exploreSlice.reducer
 });
 export default rootReducer;
