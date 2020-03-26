e688a5926842dff2f21f7467b609a20cb76fa4d9
Benjamin Lu <lu.s.benjamin@gmail.com>
2020-03-26 09:44:23 -0700

    refactor display components

--- src/window_main/components/matches/filters.tsx
+++ src/window_main/components/matches/filters.tsx
@@ -3,7 +3,7 @@ import matchSorter from "match-sorter";
 import React from "react";
 import { ColumnInstance, FilterValue, Row } from "react-table";
 import { MANA, RANKS } from "../../../shared/constants";
-import { RankSymbol } from "../misc/display";
+import { RankSymbol } from "../misc/RankSymbol";
 import { BinaryColumnFilter, BinaryFilterValue } from "../tables/filters";
 import { useMultiSelectFilter } from "../tables/hooks";
 import { MultiSelectFilterProps } from "../tables/types";
