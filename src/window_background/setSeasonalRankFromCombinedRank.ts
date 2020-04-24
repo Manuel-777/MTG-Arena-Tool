e0c52864791aa990eacb1e963ca72d529d21aee9
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-04-24 12:28:50 -0300

    make mythic update too

--- src/window_background/setSeasonalRankFromCombinedRank.ts
+++ src/window_background/setSeasonalRankFromCombinedRank.ts
@@ -1,17 +1,25 @@
 import { InternalRank } from "../types/rank";
 import globals from "./globals";
 import { SeasonalRankData } from "../types/Season";
+import globalStore, { seasonalList } from "../shared-store";
+import { reduxAction } from "../shared-redux/sharedRedux";
+import { IPC_RENDERER } from "../shared/constants";
+import { playerDb } from "../shared/db/LocalDatabase";
 
 export default function setSeasonalRankFromCombinedRank(
   rank: InternalRank,
   id: string
 ): void {
   const playerData = globals.store.getState().playerdata;
+  const currentMatch = globalStore.currentMatch;
   const owner = globals.store.getState().appsettings.email;
 
   // get these from current match
-  const type = "constructed";
-  const timestamp = new Date().getTime();
+  const type =
+    currentMatch.gameInfo.superFormat == "SuperFormat_Constructed"
+      ? "constructed"
+      : "limited";
+  const timestamp = globals.logTime.getTime();
 
   const newJson: SeasonalRankData = {
     owner,
@@ -19,8 +27,8 @@ export default function setSeasonalRankFromCombinedRank(
     playerId: playerData.arenaId,
     rankUpdateType: type,
     id,
-    eventId: "string",
-    lastMatchId: "string",
+    eventId: currentMatch.eventId,
+    lastMatchId: currentMatch.matchId,
     newClass: rank[type].rank,
     newLevel: rank[type].tier,
     newStep: rank[type].step,
@@ -32,5 +40,19 @@ export default function setSeasonalRankFromCombinedRank(
     wasLossProtected: false
   };
 
-  console.log("SeasonalRankData", newJson);
+  if (newJson.newClass == "Mythic" && newJson.oldClass == "Mythic") {
+    newJson.oldLevel = playerData.rank[type].percentile;
+    newJson.newLevel = rank[type].percentile;
+    newJson.oldStep = playerData.rank[type].leaderboardPlace;
+    newJson.newStep = rank[type].leaderboardPlace;
+  }
+
+  //console.log("SeasonalRankData", newJson);
+
+  const newSeasonal = [...seasonalList(), newJson];
+  reduxAction(globals.store.dispatch, "SET_SEASONAL", newJson, IPC_RENDERER);
+  playerDb.upsert("", "seasonal_rank", newSeasonal);
+
+  const httpApi = require("../httpApi");
+  httpApi.httpSetSeasonal(newJson);
 }
