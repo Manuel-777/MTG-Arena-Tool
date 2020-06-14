31a57e838935579abd3c995c880cbfd40e7a26cc
Tony Kuneck <tkuneck@gmail.com>
2020-06-14 00:22:53 -0700

    cleanup some of the poorer choices

--- src/shared/utils/getSetInEventId.ts
+++ src/shared/utils/getSetInEventId.ts
@@ -3,15 +3,15 @@ import database from "../database";
 export default function getSetCodeInEventId(
   eventId: string
 ): string | undefined {
-  const _undefined = undefined;
-  if (eventId === _undefined) {
-    return undefined;
-  }
-  const setCodes = Object.keys(database.sets)
-    .filter(
-      (setName) => eventId.indexOf(database.sets[setName].arenacode) !== -1
-    )
-    .map((setName) => database.sets[setName].arenacode);
+  if (eventId) {
+      const setCodes = Object.keys(database.sets)
+        .filter(
+          (setName) => eventId.indexOf(database.sets[setName].arenacode) !== -1
+        )
+        .map((setName) => database.sets[setName].arenacode);
 
-  return setCodes[0] || undefined;
+      return setCodes[0] || undefined;
+  }
+
+  return undefined;
 }
