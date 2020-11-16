4fdb8bcfc15d50bb99109712e89b6e54ee7f6466
eyeq <eyeq800@gmail.com>
2020-11-16 11:13:34 +0900

    Update getClosestMatch.ts filter limited/brawl

--- src/background/getClosestMatch.ts
+++ src/background/getClosestMatch.ts
@@ -4,7 +4,23 @@ import {
 } from "mtgatool-shared";
 import globalStore from "../shared/store";
 
+function isLimited(format: string): boolean {
+  if(!format) {
+    return false;
+  }
+  return format.indexOf("Draft") !== -1 || format.indexOf("Sealed") !== -1;
+}
+
+function isBrawl(format: string): boolean {
+  if(!format) {
+    return false;
+  }
+  return format.indexOf("Brawl") !== -1;
+}
+
 export default function getClosestMatch(oppDeck: InternalDeck): InternalMatch | undefined {
+  const limited = isLimited(oppDeck.format);
+  const brawl = isBrawl(oppDeck.format);
   const cards = oppDeck.mainDeck.map(d => {
     const cardData = db.card(d.id);
     return cardData?.name;
@@ -15,6 +31,13 @@ export default function getClosestMatch(oppDeck: InternalDeck): InternalMatch | 
   let date = "";
   for (const i in globalStore.matches) {
     const match = globalStore.matches[i];
+    if(limited && !isLimited(match.oppDeck.format)) {
+      continue;
+    }
+    if(brawl && !isBrawl(match.oppDeck.format)) {
+      continue;
+    }
+
     const matchCards = match.oppDeck.mainDeck.map(d => {
       const cardData = db.card(d.id);
       return cardData?.name;
